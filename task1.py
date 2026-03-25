c1b8b89286600c9ee386dc03bb349d6fc92b1e71
mox098 <moxasangani1@gmail.com>
2026-03-25 18:45:42 +0000

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,31 @@
+x=7
+
+if(type(x)is int):
+    print("true")
+
+else:
+    print("false")
+
+x=6.2
+if(type(x)is not float):
+    print("true")
+
+else:
+    print("false")
+
+x=25
+y=25
+
+if(x is y):
+    print("x and y are the same identity")
+
+y=20
+
+if(x is y):
+    print("terms are alike")
+
+else:
+    print("x and y are different")
+
+          
+    
